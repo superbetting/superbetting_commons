0aa349573ef279cccee94544a6d66e7b54c73b5f
Giovanni M. Martelli <giovanni.martelli1@gmail.com>
2022-12-08 20:07:20 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='commons',
+      version='1.0',
+      description='Private commons for superbetting',
+      author='Giovanni M. Martelli',
+      author_email='giovanni.martelli1@gmail.com',
+      url='https://github.com/superbetting/commons',
+      packages=[],
+     )
