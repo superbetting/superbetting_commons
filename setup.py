19cf83c72c51ea44dbafc2b393afa1f6f1f23d01
Giovanni M. Martelli <giovanni.martelli1@gmail.com>
2022-12-08 20:28:01 +0100

    added pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='commons',
-      version='1.0',
+      version='0.0.1',
       description='Private commons for superbetting',
       author='Giovanni M. Martelli',
       author_email='giovanni.martelli1@gmail.com',
