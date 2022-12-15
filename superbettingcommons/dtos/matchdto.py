3b9197c4cb08b5e28a5818cc3c2a7f7d365e8ddc
Giovanni M. Martelli <giovanni.martelli1@gmail.com>
2022-12-15 16:49:59 +0100

    fix

--- superbettingcommons/dtos/matchdto.py
+++ superbettingcommons/dtos/matchdto.py
@@ -52,10 +52,3 @@ class MatchDto(BaseDto):
     def __init__(self):
         self.match_data = _MatchData()
         self.bet_data = _BetData()
-
-    def to_json(self):
-        return json.dumps(self, default=lambda o: dump_function(o), sort_keys=True, indent=4)
-
-    @staticmethod
-    def load_from_json(js: str):
-        return json.loads(js, object_hook=lambda d: SimpleNamespace(**d))
